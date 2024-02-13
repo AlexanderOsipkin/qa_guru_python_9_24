97c99114ae096243f5c063ef2373ea6bb08dbca3
Alexander Osipkin <aleksandrosipkin@yandex.ru>
2024-02-13 16:50:13 +0300

    adding attach and fix issues

--- kinopoisk_tests_suite/tests/web/conftest.py
+++ kinopoisk_tests_suite/tests/web/conftest.py
@@ -1,18 +1,54 @@
+import os
+
 import pytest
-from selene import browser
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import browser
+from kinopoisk_tests_suite.utils import attach
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_cfg():
+def setup_browser():
+    options = Options()
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': '100.0',
+        'selenoid:options': {'enableVNC': True, 'enableVideo': True},
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{login}:{password}@{remote_browser_url}',
+        options=options,
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://hd.kinopoisk.ru/'
-    driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-    browser.config.timeout = 15
-    browser.config.window_height = 1080
+    browser.config.timeout = 10
     browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
